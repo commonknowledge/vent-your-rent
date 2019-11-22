0d4a705620e9f0d12b4ee8317f9f5246d9f1b7b6
Jan Baykara <janbaykara@gmail.com>
2019-11-22 16:20:46 +0000

    Scan webpack build directory for template files

--- config/settings/base.py
+++ config/settings/base.py
@@ -163,7 +163,7 @@ TEMPLATES = [
         # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
-        "DIRS": [str(APPS_DIR.path("templates"))],
+        "DIRS": [str(APPS_DIR.path("templates")), str(FRONTEND_DIST_DIR)],
         "OPTIONS": {
             # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
             # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
