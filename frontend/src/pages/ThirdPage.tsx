4256d7d539e7294fa435091bfe4ef6b1b6f92932
Alex Andrews <alex@alexandrews.info>
2019-11-22 07:27:07 +0000

    Finalise third page basics

--- frontend/src/pages/ThirdPage.tsx
+++ frontend/src/pages/ThirdPage.tsx
@@ -6,6 +6,10 @@ import Emoji from "a11y-react-emoji";
 import Page from "../components/Page";
 import ShareBar from "../components/ShareBar";
 import VentsBlock from "../components/VentsBlock";
+import ContactCandidateBlock from "../components/ContactCandidateBlock";
+import DonationBlock from "../components/DonationBlock";
+import GenerationRentBlock from "../components/GenerationRentBlock";
+import RenterManifestoBlock from "../components/RenterManifestoBlock";
 
 const ThirdPage = () => (
   <Page>
@@ -15,6 +19,13 @@ const ThirdPage = () => (
     </h2>
     <ShareBar />
     <VentsBlock numberOfVents={14} showMore={true} />
+    <div>
+      <h2>Still mad?</h2>
+      <ContactCandidateBlock />
+      <DonationBlock />
+      <GenerationRentBlock />
+      <RenterManifestoBlock />
+    </div>
   </Page>
 );
 
