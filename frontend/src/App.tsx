bfb30f2f750b4b24f102494b025e5df5cb8dbc18
Alex Andrews <alex@alexandrews.info>
2019-11-22 14:16:41 +0000

    Remove commented out code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,7 @@ import { Global, css } from "@emotion/core";
 
 import emotionNormalize from "emotion-normalize";
 
-// import FirstPage from "./pages/FirstPage";
 import ResultsPage from "./pages/ResultsPage";
-// import ThirdPage from "./pages/ThirdPage";
-// import VentPane from "./components/VentPane";
 
 const App: React.FC = () => {
   return (
