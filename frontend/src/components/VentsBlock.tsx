1d3eeef1615704b1bab3b738f89f88cfaa499e31
Alex Andrews <alex@alexandrews.info>
2019-11-22 14:01:00 +0000

    Add VentsBlock as horizontally scrollable

--- frontend/src/components/VentsBlock.tsx
+++ frontend/src/components/VentsBlock.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
+import faker from "faker";
 import Button from "./Button";
 
 type VentsBlockProps = {
@@ -8,20 +9,76 @@ type VentsBlockProps = {
   showMore?: boolean;
 };
 
+const horizontalVentContainer = () => {
+  return "flex: 0 0 auto;";
+};
+
+const ventContainerCSS = css`
+  margin-top: 10px;
+  margin-right 21px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  max-width: 150px;
+  ${horizontalVentContainer()}
+`;
+
+const ventText = css`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+`;
+
+const ventDetailsCSS = css`
+  margin-top: 10px;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 10px;
+  line-height: 12px;
+  text-transform: uppercase;
+
+  color: #353535;
+`;
+
+const ventImage = css`
+  max-width: 150px;
+`;
+
 function ventsDummy(amount: number) {
   const vents: Array<JSX.Element> = [];
   for (let ventsTotal = 0; ventsTotal < amount; ventsTotal++) {
     vents.push(
-      <div key={ventsTotal}>
-        <div></div>
-        <div>Name</div>
-        <div>Location</div>
+      <div key={ventsTotal} css={ventContainerCSS}>
+        <div css={ventText}>{faker.lorem.sentence()}</div>
+        <div css={ventDetailsCSS}>
+          <div>{faker.name.firstName()}</div>
+          <div>{faker.address.city()}</div>
+        </div>
+      </div>
+    );
+  }
+
+  for (let ventsTotal = 0; ventsTotal < amount; ventsTotal++) {
+    vents.push(
+      <div key={`${ventsTotal}-image`} css={ventContainerCSS}>
+        <img css={ventImage} src="https://placeimg.com/640/480/people" alt="" />
+        <div css={ventDetailsCSS}>
+          <div>{faker.name.firstName()}</div>
+          <div>{faker.address.city()}</div>
+        </div>
       </div>
     );
   }
 
   return vents;
 }
+
+const ventsContainer = () => css`
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+`;
 
 export default function VentsBlock({
   title,
@@ -31,7 +88,7 @@ export default function VentsBlock({
   return (
     <div>
       {title && <h3>{title}</h3>}
-      <div>{ventsDummy(numberOfVents)}</div>
+      <div css={ventsContainer}>{ventsDummy(numberOfVents)}</div>
       {showMore && <Button type="outline">Load More</Button>}
     </div>
   );
