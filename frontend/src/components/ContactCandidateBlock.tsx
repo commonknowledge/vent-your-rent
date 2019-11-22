e7613cb1640c48e2522db578ed074074cc9f9f68
Alex Andrews <alex@alexandrews.info>
2019-11-22 07:26:56 +0000

    Add two call to action blocks

---
+++ frontend/src/components/ContactCandidateBlock.tsx
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import { Fragment } from "react";
+import Emoji from "a11y-react-emoji";
+import Button from "./Button";
+
+export default function ContactCandidateBlock() {
+  return (
+    <div>
+      <div>
+        {" "}
+        <Emoji symbol="🚨" /> Raise awareness of this issue by contacting your
+        local candidate directly{" "}
+      </div>
+      <Button>Meet your candidates</Button>
+      <Button>Tweet your candidates</Button>
+    </div>
+  );
+}
