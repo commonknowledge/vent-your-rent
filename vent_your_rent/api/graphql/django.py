1082a5b31212eb5e8779f4348818686a1b9c3d5b
Jan Baykara <janbaykara@gmail.com>
2019-11-22 17:38:35 +0000

    Add queries for real Vent data, as well as mockVent / mockVents for legacy mocked responses

--- vent_your_rent/api/graphql/django.py
+++ vent_your_rent/api/graphql/django.py
@@ -38,12 +38,28 @@ class VentMutation(DjangoModelFormMutation):
         form_class = VentForm
 
 class Queries():
-    vents = graphene.List(graphene.NonNull(VentType), required=True) # DjangoFilterField(VentType)
+    ## production
 
-    def resolve_vents(self, info):
+    vents = graphene.List(graphene.NonNull(VentType), required=True, quantity=graphene.Int(default_value=3)) # DjangoFilterField(VentType)
+
+    def resolve_vents(self, info, quantity = 3):
+        return Vent.objects.filter(is_published=True)\
+            .limit(quantity)\
+            .order_by('date_created')
+
+    vent = graphene.Field(VentType, id=graphene.String(required=True))
+
+    def resolve_vent(self, info, id):
+        return Vent.objects.get(id=id)
+
+    ## mocks
+
+    mock_vents = graphene.List(graphene.NonNull(VentType), required=True, quantity=graphene.Int(default_value=3))  # DjangoFilterField(VentType)
+    
+    def resolve_mock_vents(self, info, quantity = 3):
         vents = []
         n = 1
-        for _ in itertools.repeat(None, 40):
+        for _ in itertools.repeat(None, quantity):
             vents.append(Vent(
                 id=n,
                 postcode=random.choice(["LL47 6YP", "PH38 4LZ", "W5 3PB", "PA29 6TB", "TN26 1AJ"]),
@@ -53,11 +69,11 @@ class Queries():
             n = n + 1
         return vents
 
-    vent = graphene.Field(VentType, id=graphene.String(required=True))
+    mock_vent = graphene.Field(VentType, id=graphene.String(required=True))
 
-    def resolve_vent(self, info, id):
+    def resolve_mock_vent(self, info, id):
         return Vent(
-            id=n,
+            id=id,
             postcode=random.choice(["LL47 6YP", "PH38 4LZ", "W5 3PB", "PA29 6TB", "TN26 1AJ"]),
             caption=random.choice(["I hate my landlord", "Never met my landlord", "is this hell on earth"]),
             first_name=random.choice(["Alex", "Gemma", "Georgie", "Dan", "Jan"])
