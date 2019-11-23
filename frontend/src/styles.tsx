423fd3bc81a50f65793f9d077408da7938009260
Alex Andrews <alex@alexandrews.info>
2019-11-23 10:34:02 +0100

    Add simple styling utilities to make CSS more modular

--- frontend/src/styles.tsx
+++ frontend/src/styles.tsx
@@ -9,3 +9,27 @@ export const paddingCss = css`
   padding-right: 20px;
   padding-left: 20px;
 `;
+
+export const fontSizeLarge = css`
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 38px;
+  line-height: 38px;
+`;
+
+export const colorWhite = "#FFFFFF";
+
+export const equalTopAndBottomMargin = (spacing: number) => {
+  return css`
+    margin-top: ${spacing}px;
+    margin-bottom: ${spacing}px;
+  `;
+};
+
+export const equalTopAndBottomPadding = (spacing: number) => {
+  return css`
+    padding-top: ${spacing}px;
+    padding-bottom: ${spacing}px;
+  `;
+};
