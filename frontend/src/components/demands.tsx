ccd5633eaf528647125b7a475f265a6edb23d278
Alex Andrews <alex@alexandrews.info>
2019-11-24 11:50:08 +0100

    Remove unused CSS function

--- frontend/src/components/demands.tsx
+++ frontend/src/components/demands.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, css } from "@emotion/core";
+import { jsx } from "@emotion/core";
 import DemandBlock from "./DemandBlock";
 
 export const NationalDatabaseOfLandlordsAndRents = () => (
