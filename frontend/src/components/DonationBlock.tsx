c0134771328c2c09c18795898f065a54682946b4
Alex Andrews <alex@alexandrews.info>
2019-11-25 07:15:40 +0100

    Complete donation block apart from link

--- frontend/src/components/DonationBlock.tsx
+++ frontend/src/components/DonationBlock.tsx
@@ -1,15 +1,30 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
 import Emoji from "a11y-react-emoji";
 import Button from "./Button";
+import { fontSizeMedium, fontSizeSmall } from "../styles";
 
 export default function DonationBlock() {
   return (
-    <div>
-      <div>
+    <div
+      css={css`
+        margin-bottom: 20px;
+      `}
+    >
+      <div
+        css={css`
+          ${fontSizeMedium}
+          letter-spacing: -0.04em;
+        `}
+      >
         <Emoji symbol="🙏" /> Help sustain this campaign
       </div>
-      <Button>Donate></Button>
+      <p css={fontSizeSmall}>
+        We have just a handful of staff but our movement is made up of thousands
+        of people like you taking action. Can you chip in a £1 to help us run
+        more campaigns like this?
+      </p>
+      <Button type="secondary">Donate</Button>
     </div>
   );
 }
