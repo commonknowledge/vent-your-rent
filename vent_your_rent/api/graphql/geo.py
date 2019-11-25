084c2af4e7eadeb5a90d03bdb81500542e4f6c26
Alex Andrews <alex@alexandrews.info>
2019-11-25 14:15:42 +0000

    Clarify GraphQL endpoint

--- vent_your_rent/api/graphql/geo.py
+++ vent_your_rent/api/graphql/geo.py
@@ -1,6 +1,7 @@
 import graphene
 from vent_your_rent.api.engine.geo import postcode_geo
 
+
 class Coordinates(graphene.ObjectType):
     longitude = graphene.Float(required=True)
     latitude = graphene.Float(required=True)
@@ -18,26 +19,10 @@ class GeoCodes(graphene.ObjectType):
 
 
 class GeocodeResult(Coordinates):
-    # postcode = graphene.String(required=True)
-    # quality = graphene.Int(required=True)
-    # eastings = graphene.Int(required=True)
-    # northings = graphene.Int(required=True)
     country = graphene.String(required=True)
-    # nhs_ha = graphene.String(required=True)
     european_electoral_region = graphene.String(required=True)
-    # primary_care_trust = graphene.String(required=True)
     region = graphene.String(required=False)
-    # lsoa = graphene.String(required=True)
-    # msoa = graphene.String(required=True)
-    # incode = graphene.String(required=True)
-    # outcode = graphene.String(required=True)
     parliamentary_constituency = graphene.String(required=True)
-    # admin_district = graphene.String(required=True)
-    # parish = graphene.String(required=True)
-    # admin_ward = graphene.String(required=True)
-    # ccg = graphene.String(required=True)
-    # nuts = graphene.String(required=True)
-    codes = graphene.NonNull(GeoCodes)
 
 
 class Queries():
