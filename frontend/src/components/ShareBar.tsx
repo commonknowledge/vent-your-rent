8a813d5a0cc18b0904defede5023a77c62384ee8
Alex Andrews <alex@alexandrews.info>
2019-11-25 05:24:37 +0100

    Add functions for creating sharing URLs on various social networks

--- frontend/src/components/ShareBar.tsx
+++ frontend/src/components/ShareBar.tsx
@@ -1,6 +1,30 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 
+// Sharing URL functionality adapted from react-social-sharing
+// https://github.com/SaraVieira/react-social-sharing
+function facebookShareUrl(link: string) {
+  return `https://facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}`;
+}
+
+function twitterShareUrl(link: string, message: string) {
+  return `https://twitter.com/intent/tweet/?text=${encodeURIComponent(
+    message
+  )}&url=${encodeURIComponent(link)}`;
+}
+
+function whatsAppShareUrl(link: string, message: string) {
+  return `whatsapp://send?text=${encodeURIComponent(
+    message
+  )}%20${encodeURIComponent(link)}`;
+}
+
+function emailShareUrl(link: string, subject: string, body?: string) {
+  return `mailto:?subject=${encodeURIComponent(
+    subject || ""
+  )}&body=${encodeURIComponent((body && `${body}\n\n${link}`) || link)}`;
+}
+
 export default function ShareBar() {
   return (
     <div>
