231cc5207a8ccdf2aa7a3c878909347f59a853fd
Alex Andrews <alex@alexandrews.info>
2019-11-25 07:11:51 +0100

    Complete basic styling of MeetYourCandidates component

--- frontend/src/components/ContactCandidateBlock.tsx
+++ frontend/src/components/ContactCandidateBlock.tsx
@@ -1,18 +1,39 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
 import Emoji from "a11y-react-emoji";
 import Button from "./Button";
+import { smallSpacing, fontSizeMedium, fontColorDarkBlack } from "../styles";
 
 export default function ContactCandidateBlock() {
   return (
-    <div>
-      <div>
-        {" "}
+    <div
+      css={css`
+        // TODO: Make this \${smallSpacing} * 2
+        margin-bottom: 30px;
+      `}
+    >
+      <div
+        css={css`
+          ${fontSizeMedium}
+          // TODO: Add this to fontSizeMedium in general
+        letter-spacing: -0.04em;
+
+          ${fontColorDarkBlack}
+          margin-bottom: ${smallSpacing};
+        `}
+      >
         <Emoji symbol="🚨" /> Raise awareness of this issue by contacting your
-        local candidate directly{" "}
+        local candidate directly
       </div>
-      <Button>Meet your candidates</Button>
-      <Button>Tweet your candidates</Button>
+      <Button
+        additionalCSS={css`
+          margin-bottom: ${smallSpacing};
+        `}
+        type="secondary"
+      >
+        Meet your candidates
+      </Button>
+      <Button type="secondary">Tweet your candidates</Button>
     </div>
   );
 }
