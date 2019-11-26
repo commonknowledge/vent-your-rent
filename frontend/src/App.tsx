dc5a7af559627c6bfdb351b4d30d635519c00f1a
Alex Andrews <alex@alexandrews.info>
2019-11-26 06:46:35 +0000

    Remove unused dependencies and rearrange

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,64 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
-import { Global, css } from "@emotion/core";
+import { css, Global, jsx } from "@emotion/core";
 import emotionNormalize from "emotion-normalize";
-import ResultsPage from "./pages/ResultsPage";
+import { createBrowserHistory } from "history";
+import { Route, Router, Switch } from "react-router-dom";
+import { analytics, AnalyticsProvider } from "./data/analytics";
 import GraphQLProvider from "./data/graphql";
-import { Router, Switch, Route } from "react-router-dom";
-import { createBrowserHistory, History } from 'history'
-import { AnalyticsProvider, analytics } from './data/analytics'
 import FirstPage from "./pages/FirstPage";
+import ResultsPage from "./pages/ResultsPage";
 import ThirdPage from "./pages/ThirdPage";
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
 
 // @ts-ignore
-analytics.logView(window.location.pathname)
+analytics.logView(window.location.pathname);
 
 history.listen(l => {
-  analytics.logView(l.pathname)
-})
+  analytics.logView(l.pathname);
+});
 
 const App: React.FC = () => {
   return (
-    <AnalyticsProvider value={analytics} >
+    <AnalyticsProvider value={analytics}>
       <Router history={history}>
         <GraphQLProvider>
           <Global
             styles={css`
-            ${emotionNormalize}
-            body {
-              background: #f0f0f0;
-              font-family: "Rubik", sans-serif;
-              min-height: 100%;
-              margin: 0;
-              padding: 0;
-            }
+              ${emotionNormalize}
+              body {
+                background: #f0f0f0;
+                font-family: "Rubik", sans-serif;
+                min-height: 100%;
+                margin: 0;
+                padding: 0;
+              }
 
-            // Set box-sizing to border-box universally
-            // See https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/
-            html {
-              box-sizing: border-box;
-            }
+              // Set box-sizing to border-box universally
+              // See https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/
+              html {
+                box-sizing: border-box;
+              }
 
-            *,
-            *:before,
-            *:after {
-              box-sizing: inherit;
-            }
-          `}
+              *,
+              *:before,
+              *:after {
+                box-sizing: inherit;
+              }
+            `}
           />
           <Switch>
-            <Route exact path="/welcome-to-the-movement" component={ThirdPage} />
+            <Route
+              exact
+              path="/welcome-to-the-movement"
+              component={ThirdPage}
+            />
             <Route exact path="/:postcode" component={ResultsPage} />
             <Route component={FirstPage} />
           </Switch>
         </GraphQLProvider>
       </Router>
-    </AnalyticsProvider >
+    </AnalyticsProvider>
   );
 };
 
