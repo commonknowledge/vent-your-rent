cab3ea8cd6f7e327264b8ff94be16edaf13a66a8
Jan Baykara <janbaykara@gmail.com>
2020-05-07 17:49:41 +0100

    Add colours to theme file from figma

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -5,8 +5,12 @@ export default {
     monospace: 'Menlo, monospace',
   },
   colors: {
-    orange: 'orange',
-    grey: '#EEE',
+    orange: '#FF974B',
+    orangeLight: '#FFEBD8',
+    grey: '#F0F0F0',
     white: '#FFF',
+    text: '#353535',
+    textLight: '#949494',
+    black: '#000000'
   },
 }
