751b8dbb7ff28951fb224425e9a7ea8633486dcc
Jan Baykara <janbaykara@gmail.com>
2020-05-15 16:25:14 +0100

    Keep 'site by' all on one line

--- frontend/src/pages/May2020/components/Footer.tsx
+++ frontend/src/pages/May2020/components/Footer.tsx
@@ -6,7 +6,7 @@ export const FooterLinks = () => {
     <Box sx={{ my: 3, '> *': { mr: 3 }, textTransform: 'uppercase', fontSize: 0 }}>
       <a target='_blank' sx={{ color: 'black' }} href='https://www.generationrent.org/contact'><b>Contact</b></a>
       <a target='_blank' sx={{ color: 'black' }} href='https://www.generationrent.org/privacy_notice'><b>Privacy</b></a>
-      <a target='_blank' sx={{ color: 'black' }} href='https://commonknowledge.coop'>Site by <b>Common&nbsp;Knowledge</b></a>
+      <a target='_blank' sx={{ color: 'black' }} href='https://commonknowledge.coop'>Site by&nbsp;<b>Common&nbsp;Knowledge</b></a>
     </Box>
   )
 }
